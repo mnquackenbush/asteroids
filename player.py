cd458fddc4981a5d60f982c4ece57ebc6923c4d8
mnquackenbush <mnquackenbush@gmail.com>
2025-02-01 14:42:31 +1100

    Added a cooldown functionality to the fire rate of the shots

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x, y, PLAYER_RADIUS)
 		self.rotation = 0
+		self.timer = 0
 
 	def triangle(self):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -34,7 +35,10 @@ class Player(CircleShape):
 		if keys[pygame.K_s]:
 			self.move(-dt)
 		if keys[pygame.K_SPACE]:
-			self.shoot()
+			if self.timer > 0:
+				self.timer -= dt
+			else:
+				self.shoot()
 	
 	def move(self, dt):
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -43,4 +47,5 @@ class Player(CircleShape):
 	def shoot(self):
 		shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
 		shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+		self.timer = PLAYER_SHOOT_COOLDOWN
 
