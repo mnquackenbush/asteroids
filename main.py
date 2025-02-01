c48403ad23dab272ce1a3951173d31e1fd6f4610
mnquackenbush <mnquackenbush@gmail.com>
2025-02-01 11:44:44 +1100

    Moving initial files to New GitHub repository

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		screen.fill("black")
+		pygame.display.flip()
+		ms_passed = clock.tick(60)
+		dt = ms_passed / 1000
+
+if __name__ == "__main__":
+	main()
