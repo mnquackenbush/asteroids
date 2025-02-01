3d727630c30134487222457a3795710a690aa661
mnquackenbush <mnquackenbush@gmail.com>
2025-02-01 13:23:32 +1100

    Updated main.py to group sprites into 'updatable' and 'drawable' groups

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
-		player.draw(screen)
-		player.update(dt)
+		updatable.update(dt)
+		for sprite in drawable:
+			sprite.draw(screen)
 		pygame.display.flip()
 		ms_passed = clock.tick(60)
 		dt = ms_passed / 1000
