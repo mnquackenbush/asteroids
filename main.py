df11c673ca3dd3b08ec761f25b74611e38772aa4
mnquackenbush <mnquackenbush@gmail.com>
2025-02-01 14:05:14 +1100

    Adds collision method to all CircleShape objects, and detects collisions to end game

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 	pygame.init()
@@ -9,14 +12,22 @@ def main():
 	dt = 0
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	AsteroidField.containers = (updatable)
+	Asteroid.containers = (asteroids, updatable, drawable)
 	Player.containers = (updatable, drawable)
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroid_field = AsteroidField()
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
 		updatable.update(dt)
+		for object in asteroids:
+			if object.collides(player) == True:
+				print("Game over!")
+				sys.exit()
 		for sprite in drawable:
 			sprite.draw(screen)
 		pygame.display.flip()
